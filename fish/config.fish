fc5a2ba41ef34905fa7c89729a3a2a1ecc927fda
3rfaan <zubi.arfan@gmail.com>
2025-06-28 23:45:39 +0200

    Added abbreviations

--- fish/config.fish
+++ fish/config.fish
@@ -1,21 +1,23 @@
 set -U fish_greeting
 
 # Aliases
-alias ls="eza -laF --icons"
 alias diff="diff --color=auto"
+alias fzf="fzf --preview 'bat --color=always {}'"
 alias grep="grep --color=auto"
 alias ip="ip --color=auto"
-alias fzf="fzf --preview 'bat --color=always {}'"
+alias ls="eza -laF --icons"
 
+abbr c cargo
+abbr ff fastfetch
+abbr glog "git log --all --decorate --graph --oneline --stat"
 abbr q exit
 abbr vi nvim
 abbr vim nvim
-abbr glog "git log --all --decorate --graph --oneline --stat"
 
 # Environment variables
+set -Ux BAT_THEME GitHub
 set -Ux EDITOR nvim
 set -Ux VISUAL nvim
-set -Ux BAT_THEME GitHub
 
 # Start sway at login
 if status is-login
