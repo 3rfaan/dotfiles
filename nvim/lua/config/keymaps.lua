81792efee49df19d369e678ca0731959f206f50b
3rfaan <zubi.arfan@gmail.com>
2025-08-24 10:44:10 +0200

    Make comment more clear

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,7 +1,7 @@
 -- Redo
 vim.keymap.set("n", "U", "<C-r>", { noremap = true, desc = "Redo previous change" })
 
--- Windows
+-- Focus windows
 vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
