0ac0d6c8f12109f40882b4d078c922e3c7bb8a65
3rfaan <zubi.arfan@gmail.com>
2025-08-25 18:47:45 +0200

    Renamed lsp to mason in plugins

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -4,7 +4,17 @@ return {
     build = ':TSUpdate',
     config = function()
         require 'nvim-treesitter.configs'.setup({
-            ensure_installed = { "c", "go", "lua", "javascript", "html", "rust", "python" },
+            ensure_installed = {
+                "c",
+                "go",
+                "html",
+                "javascript",
+                "lua",
+                "python",
+                "query",
+                "rust"
+            },
+
             sync_install = false,
             highlight = { enable = true },
             indent = { enable = true }
