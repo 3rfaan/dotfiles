dc670141b15319dcc6de69b1573f23c33bc2e91b
3rfaan <zubi.arfan@gmail.com>
2025-08-26 01:16:23 +0200

    Added mini.jump and modified mini.move bindings

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -8,7 +8,7 @@ return {
         require("mini.files").setup()
         require("mini.git").setup()
         require("mini.icons").setup()
-        require("mini.move").setup()
+        require("mini.jump").setup()
         require("mini.pairs").setup()
         require("mini.pairs").setup()
         require("mini.pick").setup()
@@ -17,6 +17,19 @@ return {
         require("mini.surround").setup()
         require("mini.tabline").setup()
 
+        require("mini.move").setup({
+            mappings = {
+                left = '<C-h>',
+                right = '<C-l>',
+                down = '<C-j>',
+                up = '<C-k>',
+
+                line_left = '<C-h>',
+                line_right = '<C-l>',
+                line_down = '<C-j>',
+                line_up = '<C-k>'
+            }
+        })
         -- Enables icons in completion popups
         MiniIcons.tweak_lsp_kind()
     end,
