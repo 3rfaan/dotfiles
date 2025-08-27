e3390dc1a0183a6d937215037246d5ce3b20c8ff
3rfaan <zubi.arfan@gmail.com>
2025-08-27 17:06:55 +0200

    Added ghostty config and mini.animate in neovim

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -3,6 +3,7 @@ return {
     "echasnovski/mini.nvim",
     version = false,
     config = function()
+        require("mini.animate").setup()
         require("mini.completion").setup()
         require("mini.diff").setup()
         require("mini.files").setup()
