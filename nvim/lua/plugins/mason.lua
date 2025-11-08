684920b18490c831a4616fb8d721410b736a75e5
3rfaan <zubi.arfan@gmail.com>
2025-11-08 13:37:00 +0100

    Added ensure_installed option to mason

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -1,7 +1,19 @@
 -- LSP
 return {
     "mason-org/mason-lspconfig.nvim",
-    opts = {},
+    opts = {
+        ensure_installed = {
+            "clangd",
+            "cssls",
+            "html",
+            "jsonls",
+            "lua_ls",
+            "rust_analyzer",
+            "svelte",
+            "taplo",
+            "ts_ls",
+        }
+    },
     dependencies = {
         { "mason-org/mason.nvim", opts = {} },
         "neovim/nvim-lspconfig",
