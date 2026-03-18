aee7c9da8c04ac66e90ac6c6e031e5d0bab31068
3rfaan <zubi.arfan@gmail.com>
2026-03-18 01:37:45 +0100

    Use new treesitter API

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -4,25 +4,24 @@ return {
     lazy = false,
     build = ':TSUpdate',
     config = function()
-        require 'nvim-treesitter.config'.setup({
-            ensure_installed = {
-                "c",
-                "css",
-                "fish",
-                "go",
-                "html",
-                "javascript",
-                "lua",
-                "python",
-                "query",
-                "rust",
-                "svelte",
-                "typescript",
-            },
+        require('nvim-treesitter').install({
+            "c",
+            "css",
+            "fish",
+            "go",
+            "html",
+            "javascript",
+            "lua",
+            "python",
+            "query",
+            "rust",
+            "svelte",
+            "typescript",
+        })
 
-            sync_install = false,
-            highlight = { enable = true },
-            indent = { enable = true }
+        vim.api.nvim_create_autocmd('FileType', {
+            pattern = { '<filetype>' },
+            callback = function() vim.treesitter.start() end,
         })
     end
 }
