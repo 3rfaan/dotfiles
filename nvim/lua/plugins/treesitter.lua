c9591390a394ea2f5b63f9b409b9a5348383c262
3rfaan <zubi.arfan@gmail.com>
2026-03-20 14:27:16 +0100

    Switching completely to new treesitter API

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -4,7 +4,14 @@ return {
     lazy = false,
     build = ":TSUpdate",
     config = function()
-        require("nvim-treesitter").install({
+        local treesitter = require("nvim-treesitter")
+
+        treesitter.setup({
+            install_dir = vim.fn.stdpath('data') .. '/site'
+        })
+
+        -- Ensure Installed
+        treesitter.install({
             "c",
             "css",
             "fish",
@@ -18,5 +25,14 @@ return {
             "svelte",
             "typescript",
         })
+
+        -- Highlighting
+        vim.api.nvim_create_autocmd('FileType', {
+            pattern = { '<filetype>' },
+            callback = function() vim.treesitter.start() end,
+        })
+
+        -- Indentation
+        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
     end
 }
