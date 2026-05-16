be3d6c715305d3a2e59da9c37ef72e3f6420b0fa
3rfaan <zubi.arfan@gmail.com>
2026-05-16 11:04:38 +0200

    Added LSP goto keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -10,5 +10,9 @@ vim.keymap.set("n", "<leader>l", "<C-w><C-l>", { desc = "Move focus to the right
 vim.keymap.set("n", "<leader>j", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<leader>k", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
+-- Goto
+vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
+vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { desc = "Go to declaration" })
+
 -- MiniFiles
 vim.keymap.set("n", "<leader>-", function() MiniFiles.open() end, { desc = "Open MiniFiles" })
