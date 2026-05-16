c838592731776ac046406dafbc63a405d3c7f29d
3rfaan <zubi.arfan@gmail.com>
2026-05-16 21:00:43 +0200

    Consistent use of double quotes

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -4,12 +4,10 @@ vim.api.nvim_create_autocmd("LspAttach", {
   callback = function(ev)
     local client = assert(vim.lsp.get_client_by_id(ev.data.client_id))
 
-    -- Enable auto-completion. Note: Use CTRL-Y to select an item. |complete_CTRL-Y|
     if client:supports_method("textDocument/completion") then
       vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
     end
 
-    -- Auto-format ("lint") on save.
     if client:supports_method("textDocument/formatting") then
       vim.api.nvim_create_autocmd("BufWritePre", {
         group = vim.api.nvim_create_augroup("my.lsp", { clear = false }),
@@ -22,8 +20,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
       })
     end
 
-    -- Enable inlay hints if server supports it
-    if client:supports_method('textDocument/inlayHint') then
+    if client:supports_method("textDocument/inlayHint") then
       vim.lsp.inlay_hint.enable(true, { bufnr = ev.buf })
     end
   end,
