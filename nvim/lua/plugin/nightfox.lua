c146c6451bac0c3742de127702d23a0ecf548ad2
3rfaan <zubi.arfan@gmail.com>
2026-05-16 18:42:57 +0200

    Added nightfox stylings

---
+++ nvim/lua/plugin/nightfox.lua
@@ -0,0 +1,14 @@
+require("nightfox").setup({
+  options = {
+    transparent = true,
+    styles = {
+      comments = "italic",
+      conditionals = "italic",
+      constants = "bold",
+      functions = "bold",
+      keywords = "bold",
+      strings = "italic",
+      types = "bold",
+    }
+  }
+})
